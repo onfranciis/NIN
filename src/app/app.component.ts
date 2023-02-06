8e730903aba279f437958f9102e84f2fb80a854c
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-02-06 05:38:05 +0100

    Make First Major Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,111 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'NIN';
+  value = '';
+  error: boolean = false;
+  gender: string = '';
+  year: string = '';
+  month: string = '';
+  day: string = '';
+  clickedOnce: boolean = false;
+  validated: boolean = false;
+
+  verify = () => {
+    console.log(this.value);
+    const data = this.value;
+    if (data.length == 11) {
+      this.error = false;
+      this.clickedOnce = false;
+      this.validated = true;
+      this.gender = parseInt(data.slice(0, 1)) % 2 == 0 ? 'Female' : 'Male';
+      this.year = data.slice(1, 3);
+      this.month = data.slice(3, 5);
+      this.day = data.slice(5, 7);
+    } else {
+      this.error = true;
+      this.clickedOnce = true;
+      this.validated = false;
+    }
+  };
+
+  dayParser = () => {
+    const data = parseInt(this.day) < 10 ? this.day.slice(1) : this.day;
+    switch (data) {
+      case '1':
+        return `${data}st`;
+        break;
+
+      case '2':
+        return `${data}nd`;
+        break;
+
+      case '3':
+        return `${data}rd`;
+        break;
+
+      default:
+        return `${data}th`;
+        break;
+    }
+  };
+
+  monthParser = () => {
+    const data = this.month;
+    switch (data) {
+      case '01':
+        return 'January';
+        break;
+
+      case '02':
+        return 'February';
+        break;
+
+      case '03':
+        return 'March';
+        break;
+
+      case '04':
+        return 'April';
+        break;
+
+      case '05':
+        return 'May';
+        break;
+
+      case '06':
+        return 'June';
+        break;
+
+      case '07':
+        return 'July';
+        break;
+
+      case '08':
+        return 'August';
+        break;
+
+      case '09':
+        return 'September';
+        break;
+
+      case '10':
+        return 'October';
+        break;
+
+      case '11':
+        return 'November';
+        break;
+
+      case '12':
+        return 'December';
+        break;
+
+      default:
+        return '';
+        break;
+    }
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatInputModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
