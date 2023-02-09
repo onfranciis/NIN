7fbd647e337e0905117d1690c5f37f4ead3ddbb2
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-02-09 03:37:44 +0100

    fix: adjust error display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,19 +25,22 @@ export class AppComponent {
     console.log(this.value);
     const data = this.value;
     if (data.length == 11) {
-      this.clickedOnce = false;
-      this.validated = true;
       this.gender = parseInt(data.slice(0, 1)) % 2 == 0 ? 'Female' : 'Male';
       this.year = data.slice(1, 3);
       this.month = data.slice(3, 5);
       this.day = data.slice(5, 7);
-      this.checksum = data.slice(9, 10);
+      this.checksum = data.slice(10, 11);
+
       if (lt_nin_checksum(data.slice(0, 10)) == parseInt(this.checksum)) {
         // The checksum is valid
+        this.validated = true;
         this.error = false;
+        this.clickedOnce = false;
       } else {
         // The checksum is not valid
+        this.validated = false;
         this.error = true;
+        this.clickedOnce = true;
       }
     } else {
       this.error = true;
